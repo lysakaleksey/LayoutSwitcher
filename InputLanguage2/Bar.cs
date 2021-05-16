77cfecc9296fe9531f1f7a834548449ffd7e249a
Oleksiy Lysak <oleksiy.lysak@dxc.com>
2021-05-16 17:47:49 +0300

    first commit

---
+++ InputLanguage2/Bar.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace InputLanguage2
+{
+    public partial class Bar : Form
+    {
+        List<InputLanguage> inputLanguages;
+        //List<string> langTitles;
+        //List<string> langCodes;
+        string retHex;
+        int prev_index = -1;
+        int counter = 0;
+        int pos = 0;
+
+
+        // Show a Form without stealing focus
+        protected override bool ShowWithoutActivation
+        {
+            get { return true; }
+        }
+
+        public Bar()
+        {
+            InitializeComponent();
+
+            inputLanguages = new List<InputLanguage>();
+            //langTitles = new List<string>();
+            //langCodes = new List<string>();
+            foreach (InputLanguage lang in InputLanguage.InstalledInputLanguages)
+            {
+                string title = lang.Culture.Parent.NativeName.ToUpper();
+                string code = lang.Culture.ToString();
+                int id = lang.Culture.KeyboardLayoutId;
+                //langTitles.Add(title);
+                //langCodes.Add(code);
+                inputLanguages.Add(lang);
+                Debug.WriteLine("Bar. title=" + title + ", code=" + code + ", id=" + id + ", hex=" +  id.ToString("X8"));
+            }
+
+            InputLanguage currLang = InputLanguage.CurrentInputLanguage;
+            Debug.WriteLine("Bar. currLang=" + currLang.Culture);
+
+            pos = inputLanguages.IndexOf(currLang); // текущий язык
+            if (pos < inputLanguages.Count() - 1)
+                prev_index = pos + 1; 
+            else
+                prev_index = 0; // прошлый язык будет следующим в списке
+            Debug.WriteLine("Bar. pos=" + pos);
+            Debug.WriteLine("Bar. prev_index=" + prev_index);
+        }
+
+        public string GetHex()
+        {
+            Debug.WriteLine("getHex. retHex=" + retHex);
+            return retHex;
+        }
+
+        public void DoHide()
+        {
+            //string langCode = inputLanguages[pos];
+            //Debug.WriteLine("DoHide. langCode=" + langCode);
+
+            //int intValue = new CultureInfo(langCode).KeyboardLayoutId;
+            int intValue = inputLanguages[pos].Culture.KeyboardLayoutId;
+            Debug.WriteLine("DoHide. KeyboardLayoutId=" + intValue);
+
+            string hex = intValue.ToString("X8");
+            Debug.WriteLine("DoHide. hex=" + hex);
+
+            retHex = hex;
+            counter = 0;
+            Hide();
+        }
+
+        public void SetLanguage()
+        {
+            counter++;
+            lblLanguage.Text = InputLanguage.CurrentInputLanguage.Culture.Parent.NativeName.ToUpper();
+            Debug.WriteLine("SetLanguage. TextOld=" + lblLanguage.Text);
+
+            if (counter == 1) // берем предыдущий
+            {
+                int t = prev_index;
+                prev_index = pos;
+                pos = t;
+            }
+            else // если нажали второй раз
+            {
+                prev_index = pos;
+                if (pos > 0)
+                    pos -= 1;
+                else
+                    pos = inputLanguages.Count() - 1;
+            }
+
+            lblLanguage.Text = inputLanguages[pos].Culture.Parent.NativeName.ToUpper();
+            Debug.WriteLine("SetLanguage. TextNew=" + lblLanguage.Text);
+        }
+    }
+}
---
+++ InputLanguage2/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace InputLanguage2
+{
+    static class Program
+    {
+        const int HC_ACTION = 0;
+        const int WH_KEYBOARD_LL = 13;
+        const int WM_KEYDOWN = 0x0100;
+        const int WM_KEYUP = 0x0101;
+        static IntPtr HookHandle = IntPtr.Zero;
+        static Bar Bar;
+
+        static Boolean kWin, kSpace;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr SetWindowsHookEx(int idHook, KbHook lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool PostMessage(IntPtr hhwnd, uint msg, IntPtr wparam, IntPtr lparam);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr LoadKeyboardLayout(string pwszKLID, uint Flags);
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [STAThread]
+        static void Main()
+        {
+            kWin = false;
+            kSpace = false;
+            try
+            {
+                using (var proc = Process.GetCurrentProcess())
+                using (var curModule = proc.MainModule)
+                {
+                    var moduleHandle = GetModuleHandle(curModule.ModuleName);
+                    HookHandle = SetWindowsHookEx(WH_KEYBOARD_LL, IgnoreWin_Space, moduleHandle, 0);
+                }
+                Bar = new Bar();
+                BuildMenu();
+                Application.Run();
+            }
+            finally
+            {
+                UnhookWindowsHookEx(HookHandle);
+            }
+        }
+
+        static void BuildMenu()
+        {
+            NotifyIcon notifyIcon1 = new NotifyIcon();
+            ContextMenuStrip contextMenuStrip1 = new ContextMenuStrip();
+            ToolStripMenuItem exitToolStripMenuItem = new ToolStripMenuItem();
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(155, 38);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += new EventHandler(ExitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] {exitToolStripMenuItem});
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(156, 80);
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = Properties.Resources.StatusIcon;
+            notifyIcon1.Text = "KeyboardLayoutRetainer";
+            notifyIcon1.Visible = true;
+        }
+
+        static void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        static IntPtr IgnoreWin_Space(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            Boolean spacePressed = false;
+            var keyInfo = (KbHookParam)Marshal.PtrToStructure(lParam, typeof(KbHookParam));
+
+            if (nCode == HC_ACTION)
+            {
+                if ((int)wParam == WM_KEYDOWN)
+                {
+                    if (keyInfo.VkCode == (int)Keys.Space)
+                    {
+                        spacePressed = true;
+                        kSpace = true;
+                    }
+                    else
+                    {
+                        kSpace = false;
+                    }
+
+                    // нажат одновременно левый виндовс
+                    if (GetAsyncKeyState(Keys.LWin) < 0)
+                    {
+                        kWin = true;
+                    }
+                    else
+                    {
+                        kWin = false;
+                    }
+
+                    if (kWin && kSpace)
+                    {
+                        if (spacePressed)
+                        {
+                            Bar.SetLanguage();
+                            Bar.Show(); // сбивает фокус, пофиксим в конструкторе
+                            return (IntPtr)1; //just ignore the key press
+                        }
+                    }
+                }
+            }
+            if ((int)wParam == WM_KEYUP)
+            {
+                if (keyInfo.VkCode == (int)Keys.LWin)
+                {
+                    kWin = false;
+                    Bar.DoHide();
+                    string HEX = Bar.GetHex();
+                    uint WM_INPUTLANGCHANGEREQUEST = 0x0050;
+                    uint KLF_ACTIVATE = 1;
+                    PostMessage(GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, IntPtr.Zero, LoadKeyboardLayout(HEX, KLF_ACTIVATE));
+                }
+            }
+
+            return CallNextHookEx(HookHandle, nCode, wParam, lParam);
+        }
+
+        delegate IntPtr KbHook(int nCode, IntPtr wParam, [In] IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KbHookParam
+        {
+            public readonly int VkCode;
+            public readonly int ScanCode;
+            public readonly int Flags;
+            public readonly int Time;
+            public readonly IntPtr Extra;
+        }
+    }
+}
